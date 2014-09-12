f74a76e3cb07a9fbd3c1dbcabb1b495f49d96717
Patrick J. Jenkinson <pajenkin@umich.edu>
2014-09-12 00:42:46 -0400

    updated changes to background and nav

--- js/index.js
+++ js/index.js
@@ -5,25 +5,26 @@ $(document).ready(function(){
 
 var counter = 0;
 var fadeTimer = 1000;
-var duration = 5000;
-var images = ['img/DSC00229.jpg', 'img/11.jpg', 'img/18.jpg'];
+var duration = 10000;
 
 function setBackgroundImageRotation(){
-	changeBackgroundImage(images[counter]);
-	if(counter == images.length - 1){
-		counter = 0;
-	};
-	counter++;
+	var visibleImage = $(".photo-background-visible");
 	setTimeout(function(){
+		visibleImage.toggleClass('photo-background-visible photo-background-hidden')
+		if(visibleImage.is(":last-child")){
+			$('.first-photo').toggleClass('photo-background-visible photo-background-hidden');
+		}else{
+			visibleImage.next().toggleClass('photo-background-visible photo-background-hidden')
+		};
 		console.log("This is the ticker");
 		setBackgroundImageRotation();
-	}, 5000);
+	}, duration);
 };
 
-function changeBackgroundImage(src){
-	var image = new Image();
-	image.src = src;
-	//image.setAttribute('class', 'img-responsive');
-	$(".background-image-index").html(image);
-	//$(".main-container").css('background-image','url(' +  src + ')');
-};
+// function changeBackgroundImage(src){
+// 	var image = new Image();
+// 	image.src = src;
+// 	//image.setAttribute('class', 'img-responsive');
+// 	$(".background-image-index").html(image);
+// 	//$(".main-container").css('background-image','url(' +  src + ')');
+// };
